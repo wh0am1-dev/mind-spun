a568604b74db8dd8dc7bd51ccdab52c26992339d
Neko250 <neko_250@ymail.com>
2015-07-03 02:32:45 +0200

    on-the-go

--- main.lua
+++ main.lua
@@ -1,20 +1,24 @@
 -- =========================================================================================
 -- Variables
 
+debug = false
+
 window = {
-    original_width = 320,
-    original_height = 240,
-    scale = 3
+    originalWidth = 320,
+    originalHeight = 240,
+    scale = 1
 }
 
-assets = {
+res = {
     dir = "assets/",
-    imageQueue = { },
-    musicQueue = { },
-    fontQueue = { },
-    images = { },
-    music = { },
-    fonts = { }
+    imgQueue = { },
+    bgmQueue = { },
+    sfxQueue = { },
+    fntQueue = { },
+    img = { },
+    bgm = { },
+    sfx = { },
+    fnt = { }
 }
 
 game_vars = { }
@@ -25,14 +29,16 @@ levels = { }
 -- Love2D main functions
 
 function love.load()
-    love.window.setMode(window.original_width * window.scale, window.original_height * window.scale)
+    love.window.setMode(window.originalWidth * window.scale, window.originalHeight * window.scale)
 
     math.randomseed(os.time())
 
-    loadImage("menu", "menu.png")
+    loadImg("menu", "menu.png")
+    loadBgm("bgm", "bgm.mp3")
     -- loadFont("font", "DejaVuSans.ttf", 20)
-    -- loadMusic("background", "background.mp3")
-    loadResources()
+    loadRes()
+
+    res.bgm.bgm:play()
 end
 
 -- -------------------------------------------------------------
@@ -42,7 +48,11 @@ function love.update(dt) end
 -- -------------------------------------------------------------
 
 function love.draw(dt)
-    love.graphics.print("FPS: " .. love.timer.getFPS(), 5, 5)
+    love.graphics.draw(res.img.menu, 0, 0)
+
+    if debug then
+        love.graphics.print("FPS: " .. love.timer.getFPS(), 5, 5)
+    end
 end
 
 -- -------------------------------------------------------------
@@ -58,7 +68,7 @@ function love.keypressed(k)
     if k == "+" then
         if window.scale < 5 then
             window.scale = window.scale + 1
-            love.window.setMode(window.original_width * window.scale, window.original_height * window.scale)
+            love.window.setMode(window.originalWidth * window.scale, window.originalHeight * window.scale)
         end
         return
     end
@@ -67,7 +77,7 @@ function love.keypressed(k)
     if k == "-" then
         if window.scale > 1 then
             window.scale = window.scale - 1
-            love.window.setMode(window.original_width * window.scale, window.original_height * window.scale)
+            love.window.setMode(window.originalWidth * window.scale, window.originalHeight * window.scale)
         end
         return
     end
@@ -85,37 +95,50 @@ function love.quit() end
 -- Assets management
 
 function loadFont(name, src, size)
-    assets.fontQueue[name] = { src, size }
+    res.fntQueue[name] = { src, size }
 end
 
 -- -------------------------------------------------------------
 
-function loadImage(name, src)
-    assets.imageQueue[name] = src
+function loadImg(name, src)
+    res.imgQueue[name] = src
 end
 
 -- -------------------------------------------------------------
 
-function loadMusic(name, src)
-    assets.musicQueue[name] = src
+function loadBgm(name, src)
+    res.bgmQueue[name] = src
 end
 
 -- -------------------------------------------------------------
 
-function loadResources(threaded)
-    for name, pair in pairs(assets.fontQueue) do
-        assets.fonts[name] = love.graphics.newFont(assets.dir .. pair[1], pair[2])
-        assets.fontQueue[name] = nil
+function loadSfx(name, src)
+    res.sfxQueue[name] = src
+end
+
+-- -------------------------------------------------------------
+
+function loadRes(threaded)
+    for name, pair in pairs(res.fntQueue) do
+        res.fnt[name] = love.graphics.newFont(res.dir .. pair[1], pair[2])
+        res.fntQueue[name] = nil
     end
 
-    for name, src in pairs(assets.imageQueue) do
-        assets.images[name] = love.graphics.newImage(assets.dir .. src)
-        assets.imageQueue[name] = nil
+    for name, src in pairs(res.imgQueue) do
+        res.img[name] = love.graphics.newImage(res.dir .. src)
+        res.imgQueue[name] = nil
     end
 
-    for name, src in pairs(assets.musicQueue) do
-        assets.music[name] = love.audio.newSource(assets.dir .. src)
-        assets.musicQueue[name] = nil
+    for name, src in pairs(res.bgmQueue) do
+        res.bgm[name] = love.audio.newSource(res.dir .. src)
+        res.bgm[name]:setLooping(true)
+        res.bgmQueue[name] = nil
+    end
+
+    for name, src in pairs(res.sfxQueue) do
+        res.sfx[name] = love.audio.newSource(res.dir .. src)
+        res.bgm[name]:setLooping(false)
+        res.sfxQueue[name] = nil
     end
 end
 
