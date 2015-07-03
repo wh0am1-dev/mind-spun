418d5e2e9a21c116c23f88627b2f4b6cc4e5405a
Neko250 <neko_250@ymail.com>
2015-07-03 02:15:35 +0200

    code redesigned

--- main.lua
+++ main.lua
@@ -1,63 +1,122 @@
-require("intro")
-require("util/resources")
-require("util/gamestack")
+-- =========================================================================================
+-- Variables
 
-resources = Resources("assets/")
-scale = 1
+window = {
+    original_width = 320,
+    original_height = 240,
+    scale = 3
+}
 
-function reset()
-    -- start game
-    intro = Intro()
-    stack = GameStack()
-    stack:push(intro)
-end
+assets = {
+    dir = "assets/",
+    imageQueue = { },
+    musicQueue = { },
+    fontQueue = { },
+    images = { },
+    music = { },
+    fonts = { }
+}
+
+game_vars = { }
+
+levels = { }
+
+-- =========================================================================================
+-- Love2D main functions
 
 function love.load()
+    love.window.setMode(window.original_width * window.scale, window.original_height * window.scale)
+
     math.randomseed(os.time())
 
-    -- load images
-    resources:addImage("author", "author.png")
-    -- load fonts
-    resources:addFont("font", "DejaVuSans.ttf", 20)
-    -- load music
-    -- resources:addMusic("background", "background.mp3")
-    resources:load()
-
-    reset()
+    loadImage("menu", "menu.png")
+    -- loadFont("font", "DejaVuSans.ttf", 20)
+    -- loadMusic("background", "background.mp3")
+    loadResources()
 end
 
-function love.update(dt)
-    stack:update(dt)
-end
+-- -------------------------------------------------------------
+
+function love.update(dt) end
+
+-- -------------------------------------------------------------
 
 function love.draw(dt)
-    love.graphics.setShader(myShader)
-    stack:draw(dt)
-    love.graphics.setShader()
-
-    love.graphics.setFont(resources.fonts.font)
     love.graphics.print("FPS: " .. love.timer.getFPS(), 5, 5)
 end
 
+-- -------------------------------------------------------------
+
 function love.keypressed(k)
-    stack:keypressed(k)
+    -- quit the game
+    if k == "escape" then
+        love.event.push("quit")
+        return
+    end
+
+    -- scale window up
+    if k == "+" then
+        if window.scale < 5 then
+            window.scale = window.scale + 1
+            love.window.setMode(window.original_width * window.scale, window.original_height * window.scale)
+        end
+        return
+    end
+
+    -- scale window down
+    if k == "-" then
+        if window.scale > 1 then
+            window.scale = window.scale - 1
+            love.window.setMode(window.original_width * window.scale, window.original_height * window.scale)
+        end
+        return
+    end
 end
 
-function love.keyreleased(k)
-    stack:keyreleased(k)
+-- -------------------------------------------------------------
+
+function love.keyreleased(k) end
+function love.mousepressed(x, y, button) end
+function love.mousereleased(x, y, button) end
+function love.mousemoved(x, y, dx, dy) end
+function love.quit() end
+
+-- =========================================================================================
+-- Assets management
+
+function loadFont(name, src, size)
+    assets.fontQueue[name] = { src, size }
 end
 
-function love.mousepressed(x, y, button)
-    stack:mousepressed(x, y, button)
+-- -------------------------------------------------------------
+
+function loadImage(name, src)
+    assets.imageQueue[name] = src
 end
 
-function love.mousereleased(x, y, button)
-    stack:mousereleased(x, y, button)
+-- -------------------------------------------------------------
+
+function loadMusic(name, src)
+    assets.musicQueue[name] = src
 end
 
-function love.mousemoved(x, y, dx, dy)
-    stack:mousemoved(x, y, dx, dy)
+-- -------------------------------------------------------------
+
+function loadResources(threaded)
+    for name, pair in pairs(assets.fontQueue) do
+        assets.fonts[name] = love.graphics.newFont(assets.dir .. pair[1], pair[2])
+        assets.fontQueue[name] = nil
+    end
+
+    for name, src in pairs(assets.imageQueue) do
+        assets.images[name] = love.graphics.newImage(assets.dir .. src)
+        assets.imageQueue[name] = nil
+    end
+
+    for name, src in pairs(assets.musicQueue) do
+        assets.music[name] = love.audio.newSource(assets.dir .. src)
+        assets.musicQueue[name] = nil
+    end
 end
 
-function love.quit()
-end
+-- =========================================================================================
--- util/gamestate.lua
+++ util/gamestate.lua
@@ -1,17 +1,1 @@
--- game state
 
-require("util/helper")
-
-GameState = class("GameState")
-
-function GameState:__init() end
-
-function GameState:update(dt) end
-function GameState:draw(dt) end
-function GameState:start() end
-function GameState:stop() end
-function GameState:keypressed(k) end
-function GameState:keyreleased(k) end
-function GameState:mousepressed(x, y, button) end
-function GameState:mousereleased(x, y, button) end
-function GameState:mousemoved(x, y, dx, dy) end
