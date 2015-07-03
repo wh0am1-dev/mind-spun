9ed6084f3a9b2b4bfa190a3c80e11e87f2ab954d
Neko250 <neko_250@ymail.com>
2015-07-03 14:57:03 +0200

    on the go...

---
+++ assets_raw/palette.md
@@ -0,0 +1,25 @@
+# Mind-Spun color palette
+
+## Red
+- c97677
+- d07f80
+
+## Blue
+- 88d7d6
+- 92dddc
+
+-----
+
+## Green
+- add484
+- b5da8d
+
+## Purple
+- 9b73c6
+- a37bcd
+
+-----
+
+## Yellow
+- f6c878
+- f9ce85
--- main.lua
+++ main.lua
@@ -1,13 +1,8 @@
+require("lib/lovedebug")
+flux = require("lib/flux")
+
 -- =========================================================================================
 -- Variables
-
-debug = false
-
-window = {
-    originalWidth = 320,
-    originalHeight = 240,
-    scale = 1
-}
 
 res = {
     dir = "assets/",
@@ -21,7 +16,30 @@ res = {
     fnt = { }
 }
 
-game_vars = { }
+button = {
+    play = {
+        name = "play",
+        x = 0,
+        y = 0,
+        width = 0,
+        height = 0,
+        up = nil,
+        down = nil,
+        wasDown = false,
+        isDown = false,
+        justPressed = false
+    }
+}
+
+global = {
+    debug = false,
+    width = 320,
+    height = 240,
+    scaleBefore = 3,
+    scale = 3,
+    fullscreen = false,
+    inMenu = true
+}
 
 levels = { }
 
@@ -29,30 +47,58 @@ levels = { }
 -- Love2D main functions
 
 function love.load()
-    love.window.setMode(window.originalWidth * window.scale, window.originalHeight * window.scale)
+    love.window.setMode(global.width * global.scale, global.height * global.scale)
     love.graphics.setDefaultFilter("nearest", "nearest", 0)
 
     math.randomseed(os.time())
 
-    loadImg("menu", "menu.png")
-    loadBgm("bgm", "bgm.mp3")
+    loadImg("menuBack", "menu.png")
+    loadImg("btnPlayUp", "btn_play_up.png")
+    loadImg("btnPlayDown", "btn_play_down.png")
+    loadBgm("menu", "menu.mp3")
     -- loadFont("font", "DejaVuSans.ttf", 20)
     loadRes()
 
-    res.bgm.bgm:play()
+    button.play.up = res.img.btnPlayUp
+    button.play.down = res.img.btnPlayDown
+    button.play.width = button.play.up:getWidth()
+    button.play.height = button.play.up:getHeight()
+    button.play.x = global.width / 2 - (button.play.up:getWidth() / 2)
+    button.play.y = global.height * 3 / 4 - (button.play.down:getHeight() / 2)
+
+    res.bgm.menu:play()
 end
 
 -- -------------------------------------------------------------
 
-function love.update(dt) end
+function love.update(dt)
+    for key, btn in pairs(button) do
+        if btn.wasDown and not btn.isDown then btn.justPressed = true end
+        btn.wasDown = btn.isDown
+    end
+end
 
 -- -------------------------------------------------------------
 
 function love.draw(dt)
-    love.graphics.draw(res.img.menu, 0, 0, 0, window.scale, window.scale)
+    if global.inMenu then
+        love.graphics.draw(res.img.menuBack, 0, 0, 0, global.scale, global.scale)
 
-    if debug then
+        if button.play.isDown then
+            love.graphics.draw(button.play.down, button.play.x * global.scale, button.play.y * global.scale, 0, global.scale, global.scale)
+        else
+            love.graphics.draw(button.play.up, button.play.x * global.scale, button.play.y * global.scale, 0, global.scale, global.scale)
+        end
+    else
+
+    end
+
+    if global.debug then
+        love.graphics.setColor(0, 0, 0, 255)
         love.graphics.print("FPS: " .. love.timer.getFPS(), 5, 5)
+        love.graphics.print("isPlayDown: " .. tostring(button.play.isDown), 5, 20)
+        love.graphics.print("playJustPressed: " .. tostring(button.play.justPressed), 5, 35)
+        love.graphics.setColor(255, 255, 255, 255)
     end
 end
 
@@ -67,30 +113,69 @@ function love.keypressed(k)
 
     -- scale window up
     if k == "+" then
-        if window.scale < 5 then
-            window.scale = window.scale + 1
-            love.window.setMode(window.originalWidth * window.scale, window.originalHeight * window.scale)
+        if not global.fullscreen and global.scale < 5 then
+            global.scale = global.scale + 1
+            love.window.setMode(global.width * global.scale, global.height * global.scale)
         end
         return
     end
 
     -- scale window down
     if k == "-" then
-        if window.scale > 1 then
-            window.scale = window.scale - 1
-            love.window.setMode(window.originalWidth * window.scale, window.originalHeight * window.scale)
+        if not global.fullscreen and global.scale > 1 then
+            global.scale = global.scale - 1
+            love.window.setMode(global.width * global.scale, global.height * global.scale)
         end
         return
+    end
+
+    -- toggle fullscreen
+    if k == "return" and love.keyboard.isDown("lalt", "ralt", "alt") then
+        global.fullscreen = not global.fullscreen
+        if global.fullscreen then
+            global.scaleBefore = global.scale
+            global.scale = 5
+        else
+            global.scale = global.scaleBefore
+        end
+        love.window.setMode(global.width * global.scale, global.height * global.scale, { fullscreen = global.fullscreen })
+    end
+end
+
+-- -------------------------------------------------------------
+
+function love.mousepressed(x, y, b)
+    if b == "l" then
+        for key, btn in pairs(button) do
+            if boxHit(x, y, btn.x * global.scale, btn.y * global.scale, btn.width * global.scale, btn.height * global.scale) then
+                btn.isDown = true
+            end
+        end
+    end
+end
+
+-- -------------------------------------------------------------
+
+function love.mousereleased(x, y, b)
+    if b == "l" then
+        for key, btn in pairs(button) do
+            if btn.isDown then btn.isDown = false end
+        end
     end
 end
 
 -- -------------------------------------------------------------
 
 function love.keyreleased(k) end
-function love.mousepressed(x, y, button) end
-function love.mousereleased(x, y, button) end
 function love.mousemoved(x, y, dx, dy) end
 function love.quit() end
+
+-- =========================================================================================
+-- Is mouse on box ?
+
+function boxHit(mx, my, x, y, w, h)
+    return mx > x and mx < x + w and my > y and my < y + h
+end
 
 -- =========================================================================================
 -- Assets management
