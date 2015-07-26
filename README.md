1c89400cac5a4fb1b2ae0890ddb4432ada183f00
Neko250 <neko_250@ymail.com>
2015-07-26 03:31:51 +0200

    alpha is here

--- README.md
+++ README.md
@@ -8,3 +8,16 @@ Puzzle game made with Love2D
 - [General Love2D info](http://gamedevelopment.tutsplus.com/articles/how-to-learn-love-love2d--gamedev-4331)
 - [Level format](http://webspace.ship.edu/deensley/flash/LeapinLizards.html)
 - [Solutions](http://webspace.ship.edu/deensley/flash/Lizards.pdf)
+
+## Beta Testers
+
+1. Manolo Reyes
+2. Nelson Casares
+3. David Bravo de Arce
+4. Jorge García Manzanares
+5. Rosa Ruiz Cabañes
+6.
+7.
+8.
+9.
+10.
--- main.lua
+++ main.lua
@@ -5,7 +5,6 @@ require("levels")
 -- =========================================================================================
 -- Variables
 
--- resources
 res = {
     dir = "assets/",
     imgQueue = { },
@@ -18,7 +17,6 @@ res = {
     fnt = { }
 }
 
--- buttons
 button = {
     play = {
         name = "play",
@@ -58,8 +56,70 @@ button = {
     }
 }
 
--- global variables
+tokens = {
+    { -- 1
+        pos = 2,
+        img = nil,
+        x = 0,
+        y = 0
+    }, { -- 2
+        pos = 3,
+        img = nil,
+        x = 0,
+        y = 0
+    }, { -- 3
+        pos = 4,
+        img = nil,
+        x = 0,
+        y = 0
+    }, { -- 4
+        pos = 5,
+        img = nil,
+        x = 0,
+        y = 0
+    }, { -- 5
+        pos = 6,
+        img = nil,
+        x = 0,
+        y = 0
+    },
+    moving = 0
+}
+
+paths = { }
+moves = { }
+
+cells = {
+    { x = 189, y = 207, size = 25, move = false },
+    { x = 102, y = 158, size = 25, move = false },
+    { x = 102, y = 57, size = 25, move = false },
+    { x = 189, y = 8, size = 25, move = false },
+    { x = 275, y = 57, size = 25, move = false },
+    { x = 275, y = 158, size = 25, move = false }
+}
+
+spots = {
+    { x = 192, y = 210 },
+    { x = 105, y = 161 },
+    { x = 105, y = 60 },
+    { x = 192, y = 11 },
+    { x = 278, y = 60 },
+    { x = 278, y = 161 },
+    free = 1,
+    freeTemp = 0
+}
+
+centers = {
+    { x = 201, y = 219 },
+    { x = 114, y = 170 },
+    { x = 114, y = 69 },
+    { x = 201, y = 20 },
+    { x = 287, y = 69 },
+    { x = 287, y = 170 }
+}
+
 global = {
+    debug = false,
     borderless = false,
     width = 320,
     height = 240,
@@ -68,16 +128,15 @@ global = {
     fullscreenScale = 1,
     scaleBefore = 3,
     volume = 1,
-    inGame = false
+    inGame = false,
+    easing = "backout"
 }
 
--- settings
 settings = {
-    debug = false,
     scale = 3,
     fullscreen = false,
     sound = true,
-    level = 0
+    level = 1
 }
 
 transition = {
@@ -90,9 +149,11 @@ transition = {
 timers = {
     time = 1.0,
     inTransition = false,
+    resetting = false,
     toGame = false,
     toMenu = false,
     exit = false,
+    resetTime = 0,
     toGameTime = 0,
     toMenuTime = 0,
     exitTime = 0
@@ -131,16 +192,46 @@ function love.load()
     loadImg("backDown", "btn_back_down.png")
     loadImg("againUp", "btn_again_up.png")
     loadImg("againDown", "btn_again_down.png")
+    loadImg("game", "game.png")
     loadImg("board", "board.png")
 
+    loadImg("path_1-2", "path_1-2.png")
+    loadImg("path_1-3", "path_1-3.png")
+    loadImg("path_1-4", "path_1-4.png")
+    loadImg("path_1-5", "path_1-5.png")
+    loadImg("path_1-6", "path_1-6.png")
+    loadImg("path_2-3", "path_2-3.png")
+    loadImg("path_2-4", "path_2-4.png")
+    loadImg("path_2-5", "path_2-5.png")
+    loadImg("path_2-6", "path_2-6.png")
+    loadImg("path_3-4", "path_3-4.png")
+    loadImg("path_3-5", "path_3-5.png")
+    loadImg("path_3-6", "path_3-6.png")
+    loadImg("path_4-5", "path_4-5.png")
+    loadImg("path_4-6", "path_4-6.png")
+    loadImg("path_5-6", "path_5-6.png")
+
+    loadImg("move_1", "move_1.png")
+    loadImg("move_2", "move_2.png")
+    loadImg("move_3", "move_3.png")
+    loadImg("move_4", "move_4.png")
+    loadImg("move_5", "move_5.png")
+    loadImg("move_6", "move_6.png")
+
+    loadImg("token_1", "token_1.png")
+    loadImg("token_2", "token_2.png")
+    loadImg("token_3", "token_3.png")
+    loadImg("token_4", "token_4.png")
+    loadImg("token_5", "token_5.png")
+
     loadBgm("music", "music.mp3")
-    loadSfx("meta", "metamorph.ogg")
-    loadSfx("phase", "phase.ogg")
+    loadSfx("yaay", "yaay.ogg")
     loadSfx("select_0", "select_0.ogg")
     loadSfx("select_1", "select_1.ogg")
     loadSfx("select_2", "select_2.ogg")
     loadSfx("select_3", "select_3.ogg")
-    -- loadFont("font", "DejaVuSans.ttf", 20)
+
+    loadFont("font", "xaba.ttf", 32)
     loadRes()
 
     -- setup objects
@@ -149,7 +240,7 @@ function love.load()
     button.play.width = button.play.up:getWidth()
     button.play.height = button.play.up:getHeight()
     button.play.x = global.width / 2 - (button.play.up:getWidth() / 2)
-    button.play.y = global.height * 3 / 4 - (button.play.down:getHeight() / 2)
+    button.play.y = global.height * 3 / 4 - (button.play.down:getHeight() / 2) - 10
     button.back.up = res.img.backUp
     button.back.down = res.img.backDown
     button.back.width = button.back.up:getWidth()
@@ -159,6 +250,14 @@ function love.load()
     button.again.width = button.again.up:getWidth()
     button.again.height = button.again.up:getHeight()
 
+    tokens[1].img = res.img.token_1
+    tokens[2].img = res.img.token_2
+    tokens[3].img = res.img.token_3
+    tokens[4].img = res.img.token_4
+    tokens[5].img = res.img.token_5
+
+    loadLevel()
+    love.graphics.setFont(res.fnt.font)
     if settings.sound then res.bgm.music:play() end
 end
 
@@ -169,7 +268,9 @@ function love.update(dt)
 
     if not timers.inTransition then
         if global.inGame then
-            updateGame(dt)
+            if not timers.resetting then
+                updateGame(dt)
+            end
         else
             updateMenu(dt)
         end
@@ -197,6 +298,9 @@ function updateGame(dt)
         button.again.isDown = false
         button.again.wasDown = false
         button.again.justPressed = true
+        timers.resetting = true
+        resetLevel()
+        resetTransition()
     end
     button.again.wasDown = button.again.isDown
 
@@ -244,6 +348,9 @@ function updateTimers(dt)
             timers.toGameTime = 0
             timers.toGame = false
             timers.inTransition = false
+            resetLevel()
+            timers.resetting = true
+            resetTransition()
         end
     elseif timers.toMenu then
         timers.toMenuTime = timers.toMenuTime + dt
@@ -257,6 +364,12 @@ function updateTimers(dt)
             timers.toMenu = false
             timers.inTransition = false
         end
+    elseif timers.resetting then
+        timers.resetTime = timers.resetTime + dt
+        if timers.resetTime > timers.time then
+            timers.resetTime = 0
+            timers.resetting = false
+        end
     end
 end
 
@@ -264,51 +377,227 @@ end
 
 function love.draw(dt)
     if global.inGame then
-        love.graphics.draw(res.img.board, 0, 0, 0, settings.scale, settings.scale)
-
-        if button.back.isDown then
-            love.graphics.draw(button.back.down, button.back.x * settings.scale, button.back.y * settings.scale, 0, settings.scale, settings.scale)
-        else
-            love.graphics.draw(button.back.up, button.back.x * settings.scale, button.back.y * settings.scale, 0, settings.scale, settings.scale)
-        end
-
-        if button.again.isDown then
-            love.graphics.draw(button.again.down, button.again.x * settings.scale, button.again.y * settings.scale, 0, settings.scale, settings.scale)
-        else
-            love.graphics.draw(button.again.up, button.again.x * settings.scale, button.again.y * settings.scale, 0, settings.scale, settings.scale)
-        end
+        drawGame(dt)
     else
-        love.graphics.draw(res.img.menu, 0, 0, 0, settings.scale, settings.scale)
-
-        if button.play.isDown then
-            love.graphics.draw(button.play.down, button.play.x * settings.scale, button.play.y * settings.scale, 0, settings.scale, settings.scale)
-        else
-            love.graphics.draw(button.play.up, button.play.x * settings.scale, button.play.y * settings.scale, 0, settings.scale, settings.scale)
-        end
+        drawMenu(dt)
     end
 
     if timers.inTransition then
         drawTransition()
     end
 
-    if settings.debug then
+    if global.debug then
         love.graphics.setColor(0, 0, 0, 255)
-        love.graphics.print("FPS: " .. love.timer.getFPS(), 5, 5)
-        if global.inGame then
-            love.graphics.print("isPlayDown: " .. tostring(button.play.isDown), 5, 20)
-            love.graphics.print("playJustPressed: " .. tostring(button.play.justPressed), 5, 35)
+        love.graphics.print("--- Debug ---", 5, 5)
+        love.graphics.print("FPS: " .. love.timer.getFPS(), 5, 20)
+
+        if not global.inGame then
+            love.graphics.print("isPlayDown: " .. tostring(button.play.isDown), 5, 35)
+            love.graphics.print("playJustPressed: " .. tostring(button.play.justPressed), 5, 50)
+        else
+            love.graphics.print("level: " .. tostring(settings.level), 5, 35)
+            love.graphics.print("isBackDown: " .. tostring(button.back.isDown), 5, 50)
+            love.graphics.print("backJustPressed: " .. tostring(button.back.justPressed), 5, 65)
+            love.graphics.print("isAgainDown: " .. tostring(button.again.isDown), 5, 80)
+            love.graphics.print("againJustPressed: " .. tostring(button.again.justPressed), 5, 95)
         end
+
         love.graphics.setColor(255, 255, 255, 255)
     end
 end
 
 -- -------------------------------------------------------------
 
+function drawMenu(dt)
+    love.graphics.draw(res.img.menu, 0, 0, 0, settings.scale, settings.scale)
+
+    if button.play.isDown then
+        love.graphics.draw(button.play.down, button.play.x * settings.scale, button.play.y * settings.scale, 0, settings.scale, settings.scale)
+    else
+        love.graphics.draw(button.play.up, button.play.x * settings.scale, button.play.y * settings.scale, 0, settings.scale, settings.scale)
+    end
+
+    love.graphics.setColor(68, 68, 68, 255)
+    love.graphics.print("Level " .. settings.level, (global.width / 2 - (res.fnt.font:getWidth("Level " .. settings.level) / 2)) * settings.scale, (global.height * 3 / 4 + 15) * settings.scale, 0, settings.scale, settings.scale)
+    love.graphics.setColor(255, 255, 255, 255)
+end
+
+-- -------------------------------------------------------------
+
+function drawGame(dt)
+    love.graphics.draw(res.img.game, 0, 0, 0, settings.scale, settings.scale)
+    drawPaths(dt)
+    love.graphics.draw(res.img.board, 0, 0, 0, settings.scale, settings.scale)
+    drawMoves(dt)
+    drawTokens(dt)
+
+    if button.back.isDown then
+        love.graphics.draw(button.back.down, button.back.x * settings.scale, button.back.y * settings.scale, 0, settings.scale, settings.scale)
+    else
+        love.graphics.draw(button.back.up, button.back.x * settings.scale, button.back.y * settings.scale, 0, settings.scale, settings.scale)
+    end
+
+    if button.again.isDown then
+        love.graphics.draw(button.again.down, button.again.x * settings.scale, button.again.y * settings.scale, 0, settings.scale, settings.scale)
+    else
+        love.graphics.draw(button.again.up, button.again.x * settings.scale, button.again.y * settings.scale, 0, settings.scale, settings.scale)
+    end
+
+    love.graphics.setColor(68, 68, 68, 255)
+    love.graphics.print("Level " .. settings.level, (57 - (res.fnt.font:getWidth("Level " .. settings.level) / 2)) * settings.scale, (global.height * 3 / 5) * settings.scale, 0, settings.scale, settings.scale)
+    love.graphics.setColor(255, 255, 255, 255)
+end
+
+-- -------------------------------------------------------------
+
+function drawTokens(dt)
+    for i = 1, 5 do
+        love.graphics.draw(res.img["token_" .. i], tokens[i].x * settings.scale, tokens[i].y * settings.scale, 0, settings.scale, settings.scale)
+    end
+end
+
+-- -------------------------------------------------------------
+
+function calculatePaths()
+    paths = { }
+
+    for i = 1, 5 do
+        for j = i + 1, 6 do
+            if levels[settings.level].paths[i][j] == 1 then
+                table.insert(paths, res.img["path_" .. i .. "-" .. j])
+            end
+        end
+    end
+end
+
+function drawPaths(dt)
+    for i, img in ipairs(paths) do
+        love.graphics.draw(img, 0, 0, 0, settings.scale, settings.scale)
+    end
+end
+
+-- -------------------------------------------------------------
+
+function calculateMoves()
+    moves = { }
+    tokens.moving = 0
+
+    for i = 1, 6 do
+        cells[i].move = false
+        if levels[settings.level].paths[spots.free][i] == 1 then
+            table.insert(moves, i)
+            cells[i].move = true
+        end
+    end
+end
+
+function drawMoves(dt)
+    for i, n in ipairs(moves) do
+        love.graphics.draw(res.img["move_" .. n], 0, 0, 0, settings.scale, settings.scale)
+    end
+end
+
+-- -------------------------------------------------------------
+
+function checkWin()
+    for i, token in ipairs(tokens) do
+        if not (i == token.pos - 1) then
+            return false
+        end
+    end
+
+    nextLevel()
+    return true
+end
+
+-- -------------------------------------------------------------
+
+function nextLevel()
+    res.sfx.yaay:setVolume(0.5)
+    res.sfx.yaay:play()
+
+    if settings.level < 40 then
+        settings.level = settings.level + 1
+    else
+        settings.level = 1
+    end
+
+    resetLevel()
+
+    timers.resetting = true
+    resetTransition()
+end
+
+-- -------------------------------------------------------------
+
+function previousLevel()
+    if settings.level > 1 then
+        settings.level = settings.level - 1
+    else
+        settings.level = 40
+    end
+
+    resetLevel()
+
+    timers.resetting = true
+    resetTransition()
+end
+
+-- -------------------------------------------------------------
+
+function resetLevel()
+    calculatePaths()
+
+    for i = 2, 6 do
+        tokens[levels[settings.level].pos[i]].pos = i
+    end
+
+    spots.free = 1
+    spots.freeBefore = 0
+    calculateMoves()
+end
+
+-- -------------------------------------------------------------
+
+function loadLevel()
+    calculatePaths()
+
+    for i = 2, 6 do
+        tokens[levels[settings.level].pos[i]].pos = i
+        tokens[levels[settings.level].pos[i]].x = spots[i].x
+        tokens[levels[settings.level].pos[i]].y = spots[i].y
+    end
+
+    spots.free = 1
+    spots.freeBefore = 0
+    calculateMoves()
+end
+
+-- -------------------------------------------------------------
+
+function getTokenOnCell(n)
+    for i, token in ipairs(tokens) do
+        if token.pos == n then
+            return i
+        end
+    end
+    return 0
+end
+
+-- -------------------------------------------------------------
+
+function resetTransition()
+    for i, token in ipairs(tokens) do
+        flux.to(token, 1, { x = spots[token.pos].x, y = spots[token.pos].y }):ease(global.easing)
+    end
+end
+
 function startTransition()
     flux.to(transition, 0.5, { alpha = 255 }):after(transition, 0.5, { alpha = 0 })
 end
 
 function exitTransition()
+    flux.to(global, 1, { volume = 0 })
     flux.to(transition, 1, { red = 0, green = 0, blue = 0, alpha = 255 })
 end
 
@@ -357,6 +646,14 @@ end
 -- -------------------------------------------------------------
 
 function love.keyreleased(k)
+    if global.debug and global.inGame then
+        if k == "a" then
+            previousLevel()
+        elseif k == "s" then
+            nextLevel()
+        end
+    end
+
     if not timers.toGame and not timers.toMenu then
         -- quit the game
         if k == "escape" then
@@ -369,7 +666,6 @@ function love.keyreleased(k)
                 res.sfx.select_0:play()
                 timers.inTransition = true
                 timers.exit = true
-                flux.to(global, 1, { volume = 0 })
                 exitTransition()
             end
             return
@@ -406,6 +702,16 @@ function love.mousereleased(x, y, b)
         if b == "l" then
             if not timers.toMenu and button.back.isDown then button.back.isDown = false end
             if not timers.reset and button.again.isDown then button.again.isDown = false end
+
+            for i = 1, 6 do
+                if table.contains(moves, i) and boxHit(x, y, cells[i].x * settings.scale, cells[i].y * settings.scale, cells[i].size * settings.scale, cells[i].size * settings.scale) then
+                    tokens.moving = getTokenOnCell(i)
+                    spots.freeBefore = spots.free
+                    spots.free = tokens[tokens.moving].pos
+                    tokens[tokens.moving].pos = spots.freeBefore
+                    flux.to(tokens[tokens.moving], 0.7, { x = spots[spots.freeBefore].x, y = spots[spots.freeBefore].y }):ease(global.easing):oncomplete(calculateMoves):oncomplete(checkWin)
+                end
+            end
         end
     else
         if b == "l" then
@@ -461,7 +767,7 @@ end
 
 function loadRes(threaded)
     for name, pair in pairs(res.fntQueue) do
-        res.fnt[name] = love.graphics.newFont(res.dir .. "fonts/" .. pair[1], pair[2])
+        res.fnt[name] = love.graphics.newFont(res.dir .. "fnt/" .. pair[1], pair[2])
         res.fntQueue[name] = nil
     end
 
@@ -481,6 +787,18 @@ function loadRes(threaded)
         res.sfx[name]:setLooping(false)
         res.sfxQueue[name] = nil
     end
+end
+
+-- =========================================================================================
+-- Table contains
+
+function table.contains(table, element)
+  for _, value in pairs(table) do
+    if value == element then
+      return true
+    end
+  end
+  return false
 end
 
 -- =========================================================================================
@@ -561,19 +879,4 @@ function table.show(t, name, indent)
     cart, autoref = "", ""
     addtocart(t, name, indent)
     return cart .. autoref
-end
-
--- =========================================================================================
--- Debug level formatting
-
-function printLevel(num)
-    print("Level 1:")
-    for i = 1, 6 do
-        print("Pod " .. i - 1 .. ":")
-        print("Links:")
-        for j = 1, 6 do
-            print("pos(" .. i .. ", " .. j .. ") = " .. levels[num].links[i][j])
-        end
-        print("Pos: " .. levels[num].pos[i])
-    end
 end
